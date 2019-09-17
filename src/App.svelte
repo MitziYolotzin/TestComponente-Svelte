900abce0e37c0360c0d76e8a1eea0f167f1ab144
MitziYolotzin <mitziulter@gmail.com>
2019-09-17 18:01:26 -0500

    mostrar componente con temporalidad y regresa a estado oculto

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,25 @@
-
 <script>
-/* 
-mostrar, temporalidad del texto, regrese al estado inicial
-funcion que contenga el timeout
- */
+  import Elemento from "../src/componentes/Elemento.svelte";
 
-import Elemento from '../src/componentes/Elemento.svelte';
+  let elemento = { activo: false };
 
-
-	let elemento = { activo: false };
-
-	function toggle() {
-		elemento.activo = !elemento.activo;
-
-		//setTimeout(toggle,5000)
-		
-	}
-	
-	setTimeout(() => {
-  toggle()  
-},5000)
-
-
+  function toggle() {
+    elemento.activo = !elemento.activo;
+    setTimeout(() => {
+      if (elemento.activo) {
+        elemento.activo = false;
+      }
+    }, 4000);
+  }
 </script>
 
-
 <style>
+
 </style>
 
-
 {#if elemento.activo}
-	<button on:click={toggle}>
-		Ocultar
-	</button>
-	<Elemento texto={'Maya'}/>
-	
+  <button on:click={toggle}>Ocultar</button>
+  <Elemento texto={'Maya'} />
 {:else}
-	<button on:click={toggle}>
-		Mostrar
-	</button>
-	
+  <button on:click={toggle}>Mostrar</button>
 {/if}
-
-
-
